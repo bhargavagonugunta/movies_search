b5c03c74ce4c0a00da3fae8686718157fb0e103c
Bhargva <bhargavagonugunta123@gmail.com>
2023-04-12 00:02:41 +0530

    main

---
+++ src/Componets/Headder.jsx
@@ -0,0 +1,8 @@
+const Headders = ()=>{
+    return(
+        <div className="h-[70px] bg-gray-200 ">
+            <h1 className="ml-3 pt-5 text-2xl shadow-slate-500 text ">Movies Search</h1>
+        </div>
+    )
+}
+export default Headders;
---
+++ src/Componets/Result.jsx
@@ -0,0 +1,10 @@
+const Result = ()=>{
+    return(
+         <div>
+            <input type="text" placeholder="Movie Name" />
+            <input type="text" placeholder="Year" />
+            <button type="button" >Search</button>
+         </div>   
+    )
+}
+export default Result;
